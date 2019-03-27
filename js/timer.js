7bf9ae1643e8b0abbde8212ab514cf4103a64d57
Gary Porter <argot.pryer@gmail.com>
2019-03-27 16:09:34 -0700

    added jQuery to start timer on button click. switch to setInterval

--- js/timer.js
+++ js/timer.js
@@ -1,14 +1,34 @@
-    var mins = 25;  //Set the number of minutes you need
-    var secs = mins * 60;
-    var currentSeconds = 0;
-    var currentMinutes = 0;
-    setTimeout('Decrement()',1000);
+var mins = 25;  //Set the number of minutes you need
+var secs = mins * 60;
+var timerRunning = false;
+let countdown;
+function Decrement() {
+  var currentMinutes = Math.floor(secs / 60);
+  var currentSeconds = secs % 60;
+  if(currentSeconds <= 9) {
+  	currentSeconds = "0" + currentSeconds;
+  }
+  secs--;
+  document.getElementById("time").innerHTML = currentMinutes + ":" + currentSeconds; //Set the element id you need the time put into.
+  if(secs <= 0) {
+  	clearInterval(countdown);
+  }
+}
+function toggleTimer(minutes) {
+	secs = minutes * 60;
+	if (!timerRunning) {
+		timerRunning = true;
+		countdown = setInterval('Decrement()', 1000);
+	} else {
+		timerRunning = false;
+		secs = 0;
+	}
+}
 
-    function Decrement() {
-        currentMinutes = Math.floor(secs / 60);
-        currentSeconds = secs % 60;
-        if(currentSeconds <= 9) currentSeconds = "0" + currentSeconds;
-        secs--;
-        document.getElementById("time").innerHTML = currentMinutes + ":" + currentSeconds; //Set the element id you need the time put into.
-        if(secs !== -1) setTimeout('Decrement()',1000);
-    }
+
+$(document).ready(function() {
+	let $tomato = $(document.getElementById("tomato"));
+	$tomato.click(function() {
+		toggleTimer(25);
+	});
+});
